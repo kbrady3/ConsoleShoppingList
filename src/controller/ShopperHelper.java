223388eb5f7502c5b8c91b5efa699b8731b9bac1
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-02-25 12:28:08 -0600

    Mostly working

--- src/controller/ShopperHelper.java
+++ src/controller/ShopperHelper.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import model.Shopper;
 
 public class ShopperHelper {
-	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("UserShoppingLists");
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("ConsoleShoppingList");
 	
 	public void insertShopper(Shopper s) {
 			EntityManager em = emfactory.createEntityManager();
@@ -23,5 +25,20 @@ public class ShopperHelper {
 			EntityManager em = emfactory.createEntityManager();
 			List<Shopper> allShoppers = em.createQuery("SELECT s FROM Shopper s").getResultList();
 			return allShoppers;
+	}
+	
+	public Shopper findShopper(String nameToLookUp) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Shopper> typedQuery = em.createQuery("select sh from Shopper sh where sh.shopperName = :selectedName", Shopper.class);
+		typedQuery.setParameter("selectedName", nameToLookUp);
+		Shopper foundShopper;
+		try {
+		foundShopper = typedQuery.getSingleResult();
+		} catch (NoResultException ex) {
+		foundShopper = new Shopper(nameToLookUp);
+		}
+		em.close();
+		return foundShopper;
 	}
 }
--- src/model/ListDetails.java
+++ src/model/ListDetails.java
@@ -3,7 +3,18 @@ package model;
 import java.time.LocalDate;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="list_details")
@@ -16,19 +27,88 @@ public class ListDetails {
 	private String listName;
 	@Column(name="TRIP_DATE")
 	private LocalDate tripDate;
+	@ManyToOne (cascade=CascadeType.PERSIST)
+	@JoinColumn(name="SHOPPER_ID")
+	private Shopper shopper;
+	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.EAGER)
+	@JoinTable
+	  (
+	      name="ITEMS_ON_LIST",
+	      joinColumns={ @JoinColumn(name="LIST_ID", referencedColumnName="LIST_ID") },
+	      inverseJoinColumns={ @JoinColumn(name="ITEM_ID", referencedColumnName="ID", unique=true) }
+	  )
+    private List<ListItem> listOfItems;
+
+	
+	public ListDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	public ListDetails(int id, String listName, LocalDate tripDate, Shopper shopper, List<ListItem> listOfItems) {
+		super();
 		this.id = id;
-		
+		this.listName = listName;
+		this.tripDate = tripDate;
+		this.shopper = shopper;
+		this.listOfItems = listOfItems;
 	}
-	
+
 	public ListDetails(String listName, LocalDate tripDate, Shopper shopper, List<ListItem> listOfItems) {
-			//omitted for space but set them in your code 
-	
+		super();
+		this.listName = listName;
+		this.tripDate = tripDate;
+		this.shopper = shopper;
+		this.listOfItems = listOfItems;
 	}
-	
+
 	public ListDetails(String listName, LocalDate tripDate, Shopper shopper) {
-			//omitted for space but set them in your code 
-	
+		super();
+		this.listName = listName;
+		this.tripDate = tripDate;
+		this.shopper = shopper;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getListName() {
+		return listName;
+	}
+	public void setListName(String listName) {
+		this.listName = listName;
+	}
+	public LocalDate getTripDate() {
+		return tripDate;
+	}
+	public void setTripDate(LocalDate tripDate) {
+		this.tripDate = tripDate;
+	}
+	public Shopper getShopper() {
+		return shopper;
+	}
+	public void setShopper(Shopper shopper) {
+		this.shopper = shopper;
+	}
+
+
+	public List<ListItem> getListOfItems() {
+		return listOfItems;
+	}
+
+
+	public void setListOfItems(List<ListItem> listOfItems) {
+		this.listOfItems = listOfItems;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ShoppingListDetails [id=" + id + ", ListName=" + listName + ", tripDate=" + tripDate + ", shopper="
+				+ shopper + ", listOfItems=" + listOfItems + "]";
 	}
 }
